6496987f44c020fa32fd717a0a7e9dcb0c8e35cb
Chumpol Mokarat <chumpol_mo@rmutto.ac.th>
2023-05-16 09:29:14 +0700

    Update top_container.php

--- tpls/top_container.php
+++ tpls/top_container.php
@@ -1,4 +1,4 @@
-<div class="w3-bar w3-black w3-large" style="z-index:0">
+<div class="w3-bar w3-black w3-large w3-mobile" style="z-index:0">
   <button class="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onclick="w3_open();"><i class="fa fa-bars"></i> Menu</button>
   <span class="w3-bar-item w3-left">
     <i class="fa fa fa-users"></i>
